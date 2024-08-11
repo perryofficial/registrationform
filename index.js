4a73e1f99906fadc57d434673811a3e216efe170
perryofficial <141177292+perryofficial@users.noreply.github.com>
2024-08-11 21:54:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+const username = encodeURIComponent(process.env.MONGODB_USERNAME);
+const password = encodeURIComponent(process.env.MONGODB_PASSWORD);
+
+const dbUri = `mongodb+srv://${username}:${password}@cluster0.x0llz.mongodb.net/myDatabase?retryWrites=true&w=majority`;
+
+mongoose.connect(dbUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected...'))
+.catch(err => console.log(err));
+
+const registrationSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+});
+
+const Registration = mongoose.model("Registration", registrationSchema);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/pages/index.html");
+});
+
+app.post("/register", async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const existingUser = await Registration.findOne({ email: email });
+        if (!existingUser) {
+            const registrationData = new Registration({
+                name,
+                email,
+                password,
+            });
+            await registrationData.save();
+            res.redirect("/success");
+        } else {
+            console.log("User already exists");
+            res.redirect("/error");
+        }
+    } catch (error) {
+        console.log(error);
+        res.redirect("/error");
+    }
+});
+
+app.get("/success", (req, res) => {
+    res.sendFile(__dirname + "/pages/success.html");
+});
+
+app.get("/error", (req, res) => {
+    res.sendFile(__dirname + "/pages/error.html");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
